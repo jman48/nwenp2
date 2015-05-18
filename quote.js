8a7681e0cd3d8a2ef9d2f8855ef0794f9b88b7cb
jman48 <j.armstrong484@gmail.com>
2015-05-18 06:54:15 +0000

    Some tidy up and general error handling

--- quote.js
+++ quote.js
@@ -2,24 +2,40 @@
 var express = require('express'),
     pg = require('pg').native,
     connectionString = 'postgres://ecnbtqsyugvdxf:dxUfMx9EGB1n35Wrw30aplM7ml@ec2-107-20-152-139.compute-1.amazonaws.com:5432/d5ocpahj31f72f',
-    start = new Date(),
     port = process.env.PORT,
     client;
-console.log('Connection url: ' + connectionString);
+
 // make express handle JSON and other requests
 var bodyParser = require('body-parser');
 // use cross origin resource sharing
 var cors = require('cors');
 // instantiate app
 var app = express();
-client = new pg.Client(connectionString);
-client.connect();
 // make sure we can parse JSON
 app.use(bodyParser.json());
 // serve up files from this directory 
 app.use(express.static(__dirname));
 // make sure we use CORS to avoid cross domain problems
 app.use(cors());
+
+client = new pg.Client(connectionString);
+client.connect();
+
+app.get('/quote/all', function(req, res) {
+    client.connect();
+    var query = client.query('SELECT * FROM quotes');
+    
+    query.on('end', function(result) {
+        if(result.rows.length <= 0) {
+            res.statusCode = 404;
+            return res.send('Error 404: No quotes avaliable to get');
+        }
+        
+        res.send(result.rows);
+    });
+    
+    handleError(query, client);
+});
 
 app.get('/quote/random', function(req, res) {
     client.connect();
@@ -35,11 +51,7 @@ app.get('/quote/random', function(req, res) {
         res.send(result.rows[rand]);
     });
     
-    query.on('error', function(error) {
-        client.end();
-        res.statusCode = 500;
-        res.send('Error: ' + JSON.stringify(error));
-    });
+    handleError(query, client);
 });
 app.get('/quote/:id', function(req, res) {
     if(sanitize(req.params, res)) {
@@ -54,44 +66,42 @@ app.get('/quote/:id', function(req, res) {
         }
         res.send(result.rows[0]);
     });
-    query.on('error', function(error) {
-        client.end();
-        //No idea what happened so return 500
-        res.statusCode = 500;
-        res.send('Error: ' + JSON.stringify(error));
-    });
+    
+    handleError(query, client);
 });
 app.post('/quote', function(req, res) {
-    console.log(req.body);
+    //Check that the required data was submitted
     if(!req.body.hasOwnProperty('author') || !req.body.hasOwnProperty('text')) {
         res.statusCode = 400;
         return res.send('Error 400: Post syntax incorrect.');
+    } else if(req.body.text === '') {
+        //We understand the request and what they are trying to do but we will not try and process it because it is in the wrong format
+        res.statusCode = 422;
+        return res.send("Error 422: Quote text is required");
     }
+    
     client.connect();
     var query = client.query('SELECT MAX(quote_id) FROM quotes');
-    query.on('error', function(error) {
-        client.end();
-        res.statusCode = 500;
-        res.send('Error: ' + error);
-    });
+    
+    handleError(query, client);
+    
     query.on('end', function(result) {
-        console.log(JSON.stringify(result));
+        
         //Create the new id based on the highest id from database
         var id = (+result.rows[0].max) + 1;
         client.connect();
-        var query2 = client.query('INSERT INTO quotes(quote_id, author, text) VALUES($1, $2, $3)', [id, req.body.author, req.body.text]);
-        query2.on('end', function(result) {
+        var insertQuery = client.query('INSERT INTO quotes(quote_id, author, text) VALUES($1, $2, $3)', [id, req.body.author, req.body.text]);
+        
+        insertQuery2.on('end', function(result) {
             client.end();
             res.statusCode = 201;
             res.send('Created a new quote with id ' + id);
         });
-        query2.on('error', function(error) {
-            client.end();
-            res.statusCode = 500;
-            res.send('Error: ' + JSON.stringify(error));
-        });
+        
+        handleError(insertQuery, client);
     });
 });
+
 app.delete('/quote/:id', function(req, res) {
     if(sanitize(req.params, res)) {
         return;
@@ -103,16 +113,25 @@ app.delete('/quote/:id', function(req, res) {
         res.statusCode = 204;
         res.json(true);
     });
-    query.on('error', function(error) {
-        client.end();
-        res.statusCode = 500;
-        res.send('Error: ' + JSON.stringify(error));
-    });
+    
+    handleError(query, client);
 });
+
 // use PORT set as an environment variable
 var server = app.listen(process.env.PORT, function() {
     console.log('Listening on port %d', server.address().port);
 });
+
+/**
+ * Handle a postgressql query error
+ */
+handleError(errQuery, errClient) {
+    errQuery.on('error', function(error) {
+        errClient.end();
+        res.statusCode = 500;
+        res.send('Unkown Error: ' + JSON.stringify(error));
+    });
+}
 
 /*
  * General checks on the id param.
@@ -130,7 +149,8 @@ function sanitize(params, res, dbClient) {
         res.statusCode = 404;
         return res.send('Error 404: No quote found');
     } else if(isNaN(+params.id)) {
-        res.statusCode = 422; //We understand the request and what they are trying to do but we will not try and process it
+        //We understand the request and what they are trying to do but we will not try and process it because it is in the wrong format
+        res.statusCode = 422;
         return res.send("Error 422: id must be of type number");
     }
     return false;

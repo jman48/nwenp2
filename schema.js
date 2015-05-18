b17330977b559cc5cbb3be412efd23e3986a2197
jman48 <j.armstrong484@gmail.com>
2015-05-18 06:58:32 +0000

    Removed code to drop table if it exists

--- schema.js
+++ schema.js
@@ -6,7 +6,7 @@ var pg = require('pg').native
 client = new pg.Client(connectionString);
 client.connect();
 
-client.query('DROP TABLE IF EXIST quotes'); //Drop the old table if it exists
+//client.query('DROP TABLE IF EXIST quotes'); //Drop the old table if it exists
 
 //Create a new quotes table
 query = client.query('CREATE TABLE quotes (quote_id integer UNIQUE NOT NULL, author VARCHAR(255) UNIQUE, text VARCHAR(3000) NOT NULL)');

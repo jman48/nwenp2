619f2d6d3942df82856ed74f4e3b9f123bacbaf9
jman48 <j.armstrong484@gmail.com>
2015-05-18 04:15:22 +0000

    Added functinoality to get random quotes

--- quote.js
+++ quote.js
@@ -5,98 +5,86 @@ var express = require('express'),
     start = new Date(),
     port = process.env.PORT,
     client;
-
 console.log('Connection url: ' + connectionString);
-
 // make express handle JSON and other requests
 var bodyParser = require('body-parser');
-
 // use cross origin resource sharing
 var cors = require('cors');
-
 // instantiate app
 var app = express();
-
 client = new pg.Client(connectionString);
 client.connect();
-
 // make sure we can parse JSON
 app.use(bodyParser.json());
-
 // serve up files from this directory 
 app.use(express.static(__dirname));
 // make sure we use CORS to avoid cross domain problems
 app.use(cors());
 
 app.get('/quote/random', function(req, res) {
-  var id = Math.floor(Math.random() * quotes.length);
-  var q = quotes[id];
-  res.send(q);
-});
-
-app.get('/quote/:id', function(req, res) {
-
-    if(!req.params.hasOwnProperty('id')) {
-        res.statusCode = 400;
-        return res.send('Error 400: Post syntax incorrect.');
-    } else if(req.params.id < 0) {
-        res.statusCode = 404;
-        return res.send('Error 404: No quote found');
-    } else if (isNaN(+req.params.id)) {
-        res.statusCode = 422; //We understand the request and what they are trying to do but we will not try and process it
-        return res.send("Error 422: id must be of type number");
-    }
-
-  client.connect();
-  var query = client.query('SELECT * FROM quotes WHERE quote_id = $1', [req.params.id]);
+    client.connect();
+    var query = client.query('SELECT * FROM quotes');
     
-    query.on('end', function (result) {
-        if (result.rows.length <= 0) {
+    query.on('end', function(result) {
+        if(result.rows.length <= 0) {
+            res.statusCode = 404;
+            return res.send('Error 404: No quotes avaliable to get');
+        }
+        
+        var rand = Math.floor(Math.random() * result.rows.length);
+        res.send(result.rows[rand]);
+    });
+    
+    query.on('error', function(error) {
+        client.end();
+        res.statusCode = 500;
+        res.send('Error: ' + JSON.stringify(error));
+    });
+});
+app.get('/quote/:id', function(req, res) {
+    if(sanitize(req.params, res)) {
+        return;
+    }
+    client.connect();
+    var query = client.query('SELECT * FROM quotes WHERE quote_id = $1', [req.params.id]);
+    query.on('end', function(result) {
+        if(result.rows.length <= 0) {
             res.statusCode = 404;
             return res.send('Error 404: No quote found');
         }
-        
         res.send(result.rows[0]);
-    });  
-    
+    });
     query.on('error', function(error) {
-            client.end();
-            //No idea what happened so return 500
-            res.statusCode = 500;
-            res.send('Error: ' + JSON.stringify(error));
-        });
+        client.end();
+        //No idea what happened so return 500
+        res.statusCode = 500;
+        res.send('Error: ' + JSON.stringify(error));
+    });
 });
-
 app.post('/quote', function(req, res) {
-  console.log(req.body);
-  if(!req.body.hasOwnProperty('author') || !req.body.hasOwnProperty('text')) {
-    res.statusCode = 400;
-    return res.send('Error 400: Post syntax incorrect.');
-  }    
+    console.log(req.body);
+    if(!req.body.hasOwnProperty('author') || !req.body.hasOwnProperty('text')) {
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect.');
+    }
     client.connect();
     var query = client.query('SELECT MAX(quote_id) FROM quotes');
-
     query.on('error', function(error) {
         client.end();
         res.statusCode = 500;
         res.send('Error: ' + error);
     });
-
-    query.on('end', function (result) {
+    query.on('end', function(result) {
         console.log(JSON.stringify(result));
         //Create the new id based on the highest id from database
         var id = (+result.rows[0].max) + 1;
-        
         client.connect();
-        
         var query2 = client.query('INSERT INTO quotes(quote_id, author, text) VALUES($1, $2, $3)', [id, req.body.author, req.body.text]);
-        
         query2.on('end', function(result) {
             client.end();
             res.statusCode = 201;
             res.send('Created a new quote with id ' + id);
         });
-
         query2.on('error', function(error) {
             client.end();
             res.statusCode = 500;
@@ -104,19 +92,46 @@ app.post('/quote', function(req, res) {
         });
     });
 });
-
 app.delete('/quote/:id', function(req, res) {
-  if(quotes.length <= req.params.id) {
-    res.statusCode = 404;
-    return res.send('Error 404: No quote found');
-  }
-
-  quotes.splice(req.params.id, 1);
-  res.json(true);
+    if(sanitize(req.params, res)) {
+        return;
+    }
+    client.connect();
+    var query = client.query('DELETE FROM quotes WHERE quote_id = $1', [req.params.id]);
+    query.on('end', function(result) {
+        client.end();
+        res.statusCode = 204;
+        res.json(true);
+    });
+    query.on('error', function(error) {
+        client.end();
+        res.statusCode = 500;
+        res.send('Error: ' + JSON.stringify(error));
+    });
 });
-
 // use PORT set as an environment variable
 var server = app.listen(process.env.PORT, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
+/*
+ * General checks on the id param.
+ *
+ * Will throw error and send response
+ *
+ * @params - This is the object that should have the 'id' field. i.e req.params or req.body
+ */
+
+function sanitize(params, res, dbClient) {
+    if(!params.hasOwnProperty('id')) {
+        res.statusCode = 400;
+        return res.send('Error 400: Post syntax incorrect.');
+    } else if(params.id < 0) {
+        res.statusCode = 404;
+        return res.send('Error 404: No quote found');
+    } else if(isNaN(+params.id)) {
+        res.statusCode = 422; //We understand the request and what they are trying to do but we will not try and process it
+        return res.send("Error 422: id must be of type number");
+    }
+    return false;
+}

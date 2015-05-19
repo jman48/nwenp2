ca13b7d4b9291674ca9fe087227d3f66f36633f2
jman48 <j.armstrong484@gmail.com>
2015-05-19 05:18:07 +0000

    herokue test

--- register.js
+++ register.js
@@ -1,26 +1,37 @@
 var pg = require('pg').native,
-    connectionString = process.env.DATABASE_URL,
+    connectionString = 'postgres://ecnbtqsyugvdxf:dxUfMx9EGB1n35Wrw30aplM7ml@ec2-107-20-152-139.compute-1.amazonaws.com:5432/d5ocpahj31f72f',
     client, 
     query,
-    password = require('password-hash-and-salt');
+    hashPass = require('password-hash-and-salt');
 
 client = new pg.Client(connectionString);
 client.connect();
 
+client.query('DROP TABLE IF EXISTS user_obj');
+
 //Create a new quotes table
-query = client.query('CREATE TABLE users (user_name VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(255) UNIQUE NOT NULL)');
+query = client.query('CREATE TABLE user_obj (user_name VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(255) UNIQUE NOT NULL)');
 
 query.on('end', function(result) {
-    createUser('User1', '1234');
-    createUser('User2', 'password');
+    client.connect();
+    client.query('INSERT INTO user_obj(user_name, password) VALUES($1, $2)', 'User1', 'hash');
+//     hashPass('1234').hash(function(error, hash) {
+//         client.query('INSERT INTO user_obj(user_name, password) VALUES($1, $2)', 'User1', hash);
+//     });    
+    
+//     hashPass('password').hash(function(error, hash) {
+//         client.query('INSERT INTO user_obj(user_name, password) VALUES($1, $2)', 'User2', hash);
+//     });
+    
     client.end();
 });
 
 function createUser(userName, password) {
-    password(password).hash(function(error, hash) {
-        var insertUser = client.query('INSERT INTO users(user_name, password) VALUES($1, $2)', userName, hash);
-        query.on('end', function(result) {
-            client.end();
-        });
+    client.connect();
+    hashPass(password).hash(function(error, hash) {
+        var insertUser = client.query('INSERT INTO user_obj(user_name, password) VALUES($1, $2)', userName, hash);
+        insertUser.on('end', function(result) {
+            client.end();     
+        })
     });
 }

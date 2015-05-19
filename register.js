9ee5ac5a2a574e4667c2acc5647091c8f429f9fe
jman48 <j.armstrong484@gmail.com>
2015-05-19 03:43:58 +0000

    Cleaned up register.js file

--- register.js
+++ register.js
@@ -5,13 +5,12 @@ client = new pg.Client(connectionString);
 client.connect();
 //Create a new quotes table
 query = client.query('CREATE TABLE user (user_name VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(255) UNIQUE NOT NULL)');
-var insertUser = client.query('INSERT INTO user(user_name, password) VALUES($1, $2)', 'User1', )
+
 query.on('end', function(result) {
+    createUser('User1', '1234');
+    createUser('User2', 'password');
     client.end();
 });
-
-createUser('User1', '1234');
-createUser('User2', 'password');
 
 function createUser(userName, password) {
     password(password).hash(function(error, hash) {

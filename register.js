56ccf8c2e1e9d7c4a4a7c8efdedf231fe76bf708
jman48 <j.armstrong484@gmail.com>
2015-05-19 03:42:19 +0000

    Added register.js file

---
+++ register.js
@@ -0,0 +1,23 @@
+var pg = require('pg').native,
+    connectionString = process.env.DATABASE_URL,
+    client, query;
+client = new pg.Client(connectionString);
+client.connect();
+//Create a new quotes table
+query = client.query('CREATE TABLE user (user_name VARCHAR(50) UNIQUE NOT NULL, password VARCHAR(255) UNIQUE NOT NULL)');
+var insertUser = client.query('INSERT INTO user(user_name, password) VALUES($1, $2)', 'User1', )
+query.on('end', function(result) {
+    client.end();
+});
+
+createUser('User1', '1234');
+createUser('User2', 'password');
+
+function createUser(userName, password) {
+    password(password).hash(function(error, hash) {
+        var insertUser = client.query('INSERT INTO user(user_name, password) VALUES($1, $2)', userName, hash);
+        query.on('end', function(result) {
+            client.end();
+        });
+    });
+}

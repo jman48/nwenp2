3bdec037d80e9357393994f7a6df334b9e788981
jman48 <j.armstrong484@gmail.com>
2015-06-11 04:32:52 +0000

    Changed quote type to text

--- schema.js
+++ schema.js
@@ -7,7 +7,7 @@ client = new pg.Client(connectionString);
 client.connect();
 
 //Create a new quotes table
-query = client.query('CREATE TABLE quotes (quote_id integer UNIQUE NOT NULL, author VARCHAR(255) UNIQUE, text VARCHAR(3000) NOT NULL)');
+query = client.query('CREATE TABLE quotes (quote_id integer UNIQUE NOT NULL, author VARCHAR(255) UNIQUE, text text NOT NULL)');
 
 query.on('end', function (result) {
     client.end();
